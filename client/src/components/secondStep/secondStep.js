bf071dfdebb32ad0799e86da3e7c975bc2eee928
Ilya <enotn36@gmail.com>
2023-05-28 17:00:10 +0300

    feat: add map

--- client/src/components/secondStep/secondStep.js
+++ client/src/components/secondStep/secondStep.js
@@ -1,35 +1,38 @@
-import { animated } from 'react-spring';
+import { animated } from "react-spring";
+import MoscowMap from '../moscowMap/moscowMap.js';
 
-import './secondStep.css';
+import "./secondStep.css";
 
-import Arrow from '../arrow/arrow.js';
+import Arrow from "../arrow/arrow.js";
 
-const SecondStep = ({onChangeActiveStep, style}) => {
-    return  (
-        <animated.div className="secondStepContainer" style={style}>
-            <div className="stepHead">
-                <div className="stepDescription">
-                    <div className="boldText">
-                    Шаг 2. Территория расположения производства
-                    </div>
-                    Пожалуйста, выберите округ расположения производства
-                </div>
-            </div>
-            <div className="space"></div>
-            <div className="arrowWrapper">
-                <Arrow
-                    text="Далее"
-                    rotation={false}
-                    availabe={'true'}
-                    onChangeActiveStep={onChangeActiveStep}/>
-                <Arrow
-                    text="Назад"
-                    rotation={true}
-                    availabe={'true'}
-                    onChangeActiveStep={onChangeActiveStep}/>
-            </div>
-        </animated.div>
-    )
-}
+const SecondStep = ({ onChangeActiveStep, style }) => {
+	return (
+		<animated.div className="secondStepContainer" style={style}>
+			<div className="stepHead">
+				<div className="stepDescription">
+					<div className="boldText">
+						Шаг 2. Территория расположения производства
+					</div>
+					Пожалуйста, выберите округ расположения производства
+				</div>
+			</div>
+			<MoscowMap />
+			<div className="arrowWrapper">
+				<Arrow
+					text="Далее"
+					rotation={false}
+					availabe={"true"}
+					onChangeActiveStep={onChangeActiveStep}
+				/>
+				<Arrow
+					text="Назад"
+					rotation={true}
+					availabe={"true"}
+					onChangeActiveStep={onChangeActiveStep}
+				/>
+			</div>
+		</animated.div>
+	);
+};
 
 export default SecondStep;
